4fd8ad4b8542eb106e6a77254f390e165cf18e89
TCOE3 <TCOE3@SAPSSAP-4UTDE1V>
2023-10-11 12:19:18 +0200

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-4, 1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 55);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(214, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Day off Tracker";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.lblCounter);
+            this.panel2.Location = new System.Drawing.Point(246, 110);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(282, 163);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.ForeColor = System.Drawing.SystemColors.Info;
+            this.lblCounter.Location = new System.Drawing.Point(104, 39);
+            this.lblCounter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(65, 75);
+            this.lblCounter.TabIndex = 0;
+            this.lblCounter.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(309, 365);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Take a Day off";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto Medium", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Available Days off";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Location = new System.Drawing.Point(286, 295);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "show future days offs";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Day Off Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsFormsApp2
+{
+    
+    public partial class Form1 : Form
+    {//constants
+        string startDatestr = "2023-03-30"; //constant date of appointment (link to database)
+        string endDatestr = "2024-03-29"; //End of contract (link to database)
+        int dayCounter = 0;
+        int dayOffcounter = 5;// link to database
+
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox= false;
+        }
+        static int daysOff(DateTime currentDate, DateTime startDate, int dayOffcounter)
+        {
+            int dayCounter;
+           
+            TimeSpan difference = currentDate - startDate; //check the difference between dates
+            int daysDifference = difference.Days; // minus 2 because we are counting week days not weekends
+            dayCounter = (daysDifference - 2) / 17;
+
+            if (daysDifference == 17)
+            {
+                dayCounter++; // for every 17 days you get one day off
+            }
+
+            dayCounter -= dayOffcounter;//  subtract from the days you already took
+            if (dayCounter < 0)
+            {
+                dayCounter = 0; // if the day counter is less than 0 for readability it will be 0
+
+            }
+            return dayCounter;
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DateTime startDate = Convert.ToDateTime(startDatestr);
+            DateTime endDate = Convert.ToDateTime(endDatestr);
+            DateTime currentDate = DateTime.Now; // set
+
+            dayCounter = daysOff(currentDate, startDate, dayOffcounter);
+
+           lblCounter.Text = dayCounter.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dayCounter <= 0)
+            {
+                MessageBox.Show("You don't have enough days of\nYour Total is :" + dayCounter);
+            }
+            else
+            {
+                //take input
+                int takeDayoff = Convert.ToInt32(Interaction.InputBox("How many day?","InputBox"));
+
+                //now I gotta verify it 
+                if (takeDayoff > dayCounter)
+                {
+                    MessageBox.Show("you do not have enough days off Choose lesser days");
+                }
+                else
+                {
+                    dayCounter -= takeDayoff; // subtract from the accumulated day off
+                    dayOffcounter += takeDayoff; // add to the day off counter
+                    MessageBox.Show("Great you have taken " + takeDayoff + " day off\nyou now have " + dayCounter + " available day off left");
+                    lblCounter.Text = dayCounter.ToString();   
+                }
+            }
+
+        }
+        static int FutureDaysOff(DateTime currentDate, DateTime endDate)
+        {
+            TimeSpan futureDiff = endDate - currentDate;
+            int futureDays = futureDiff.Days;
+            int futureDayCounter = futureDays / 17;
+
+            if (futureDays == 17)// for every 17 days that pass you get 1 day off
+            {
+                futureDayCounter++;
+            }
+
+            return futureDayCounter;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime endDate = Convert.ToDateTime(endDatestr);
+            DateTime currentDate = DateTime.Now; // set
+            DateTime startDate = Convert.ToDateTime(startDatestr);
+            int futureDays = FutureDaysOff(currentDate, endDate);
+
+            if(button2.Text == "show future days offs")
+            {
+                lblCounter.Text = futureDays.ToString();
+                button2.Text = "show Available day offs";
+                label2.Text = "Future Days off from now";
+            }
+            else 
+            {
+                lblCounter.Text = daysOff(currentDate,startDate,dayOffcounter).ToString();
+                button2.Text = "show future days offs";
+                label2.Text = "Available Days off";
+            }
+
+            
+        }
+    }
+}
